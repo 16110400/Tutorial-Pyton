3c4c13f8dc147bd2ab4d4a700f0bbe1ecefe8cbc
dikot <33674816+16110400@users.noreply.github.com>
2022-04-13 13:03:56 +0700

    first commit

---
+++ 00 - Template/Main.py
@@ -0,0 +1,16 @@
+import time
+start_time = time.time()
+
+print("Halo Dunia!")
+print("Saya Dikot")  # nama
+# ini adalah comment
+c = 90
+print(c)
+print(c)
+print("lalalala")
+"""coment 
+baris baris
+comment
+"""
+# kita bisa compile python dengan yang namanya bytecode
+print(time.time() - start_time, "detik")
---
+++ 01 - Variabel/Variabel.py
@@ -0,0 +1,11 @@
+# variabel adalah tempat menyimpan data
+
+x = 20
+y = 5
+
+panjang = 100
+lebar = 20
+
+print("Nilai x = ", x)
+print("Nilai y = ", y)
+print("Nilai panjang = ", panjang)
